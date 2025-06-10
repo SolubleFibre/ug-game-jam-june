55c98b24b2d29089669ce1259a1c262e1c2be1f5
SilverFields <ithielnascimento@gmail.com>
2025-06-09 20:19:43 -0400

    Small Changes
    Just tweaked a few things, added smaller corridors, added different states to the Dark Pool (Calm, Agitated, and Active)

--- uggd-game-jam-june-2025/Scripts/Player/player.gd
+++ uggd-game-jam-june-2025/Scripts/Player/player.gd
@@ -18,10 +18,12 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		$CameraAnims.play("view_bob")
+		$FlashlightAnims.play("bob")
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		$CameraAnims.stop()
+		$FlashlightAnims.stop()
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 

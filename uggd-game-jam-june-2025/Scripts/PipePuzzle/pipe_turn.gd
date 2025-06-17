54b539cbd4677c8b61d291c6b0e5bdbb3f41315d
Gayendra <gayendra410@gmail.com>
2025-06-16 23:38:31 -0400

    feature/puzzle_1
    the player's pipes matching the puzzle's answer can now be detected

--- uggd-game-jam-june-2025/Scripts/PipePuzzle/pipe_turn.gd
+++ uggd-game-jam-june-2025/Scripts/PipePuzzle/pipe_turn.gd
@@ -1,13 +1,16 @@
 extends Control
+## Controls the ability for a single pipe tile to turn. 
+
 
 @export var pipe_textures: Array[Texture2D]
 @export var pipe_orientation: int
-
 
 func _ready():
 	$pipe_image.texture = pipe_textures[pipe_orientation]
 
 
+## Unless the array in the inspector has more than zero textures inside, 
+## rotate the image when pressed.
 func _on_button_pressed():
 	if pipe_textures.size()<1:
 		push_error("Need more than zero textures.")

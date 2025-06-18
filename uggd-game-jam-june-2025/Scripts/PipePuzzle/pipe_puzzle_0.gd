b609f0787465bc37fe3171f020ebe69b929872f5
Gayendra <gayendra410@gmail.com>
2025-06-18 17:04:53 -0400

    Ithiel's problem now
    did pipe puzzle added subviewport and computer model holding pipe puzzle. Bug with main scene where viewport takes up the whole player screen

--- uggd-game-jam-june-2025/Scripts/PipePuzzle/pipe_puzzle_0.gd
+++ uggd-game-jam-june-2025/Scripts/PipePuzzle/pipe_puzzle_0.gd
@@ -1,6 +1,6 @@
 extends Control
 
-#puzzle answer
+## This determines the correct answer to the puzzle 
 var puzzle_answer: Array[int]= [
 	1, 2, 2,
 	1, 3, 0,
@@ -8,8 +8,15 @@ var puzzle_answer: Array[int]= [
 ]
 
 
+## Player Guess corectness display
+## displays whether the player's answer they submitted is correct or wrong
+@export var puzzle_guess_checker_display: Array[Texture2D]
+
+signal disable_screen
+
 ## Load in the changes the player made to their pipes
 func _on_submit_button_pressed():
+	# load the current state of the player's tiles.
 	var pipe_0_0: int = $tile_0_0.pipe_orientation 
 	var pipe_0_1: int = $tile_0_1.pipe_orientation 
 	var pipe_0_2: int = $tile_0_2.pipe_orientation 
@@ -28,10 +35,13 @@ func _on_submit_button_pressed():
 		pipe_2_0, pipe_2_1, pipe_2_2,
 	]
 
+
 	if puzzle_answer == player_answer:
-		print("Puzzle answer matches wiht player answer")
+		#play correct sfx
+		$SubmissionDisplay.texture = puzzle_guess_checker_display[1]
+		disable_screen.emit()
+
 	
 	else:
-		print("no match.")
-		print("Puzzle answer: ",puzzle_answer)
-		print("Player answer: ", player_answer)
+		#play wrong sfx
+		$SubmissionDisplay.texture = puzzle_guess_checker_display[0]
